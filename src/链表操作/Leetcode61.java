632d07e5dd151bf565288183e9abe8c3e5633ff6
kr_li <55921715+kr-li@users.noreply.github.com>
2021-03-07 12:59:41 +0800

    update

--- src/链表操作/Leetcode61.java
+++ src/链表操作/Leetcode61.java
@@ -3,6 +3,18 @@ package 链表操作;
 public class Leetcode61 {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null) return null;
-
+        ListNode oldHead = head;
+        int n;
+        for (n = 1; oldHead.next != null; n++){
+            oldHead = oldHead.next;
+        }
+        oldHead.next = head;
+        ListNode newHead = head;
+        for (int i = 1; i != n - k % n; i++){
+            newHead = newHead.next;
+        }
+        ListNode ans = newHead.next;
+        newHead.next = null;
+        return ans;
     }
 }
