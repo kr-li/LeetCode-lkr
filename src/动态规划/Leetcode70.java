f3331cd5c436c02e3fa976d57f59406ed97aab7b
kr_li <55921715+kr-li@users.noreply.github.com>
2021-04-15 16:01:13 +0800

    update

--- src/动态规划/Leetcode70.java
+++ src/动态规划/Leetcode70.java
@@ -1,4 +1,14 @@
 package 动态规划;
 
 public class Leetcode70 {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
 }
